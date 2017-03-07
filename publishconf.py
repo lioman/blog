6edbd05e8300e76f30ba3845acbc8dcaf68c9ccc
Lioman <dev@lioman.de>
2017-03-07 19:15:41 +0100

    use https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.lioman.de'
+SITEURL = 'https://www.lioman.de'
 RELATIVE_URLS = False
 
 OUTPUT_PATH = 'public'
